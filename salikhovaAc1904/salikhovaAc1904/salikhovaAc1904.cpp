bed2f098ac2ca0fa7e429064939ab9cc2c098f7a
salikhovaais <70656998+salikhovaais@users.noreply.github.com>
2020-10-26 22:35:08 +0300

    menu

--- salikhovaAc1904/salikhovaAc1904/salikhovaAc1904.cpp
+++ salikhovaAc1904/salikhovaAc1904/salikhovaAc1904.cpp
@@ -1,11 +1,12 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 struct Pipeline
 {
-	int length;
-	int diametr;
+	float length;
+	float diametr;
 	bool remont;
 	int ident;
 };
@@ -13,26 +14,62 @@ struct KC
 {
 	int ident;
 	string name;
-	int zeh;
-	int workzeh;
-	string eff;
+	int zeh; // кол-во цехов
+	int workzeh; // кол-во работающих цехов
+	string eff;//эффективность 
 };
-Pipeline LoadPipeline()            //создание трубы
+Pipeline LoadPipeline()            //загрузка в файл  трубы
 {
 	ifstream fin;
 	fin.open("data.txt", ios::in);
-	Pipeline x;
-	x.ident = 0;
-	x.remont = false;
-	//cout << "type length\n";
-	fin >> x.length ;
-	//cout << "type diametr\n";
-	fin >> x.diametr;
-	fin.close();
-	return x;
+	/*if (fin.is_open())*/
+	
+		Pipeline x;
+		x.ident = 0;
+		fin >> x.remont;
+		//cout << "type length\n";
+		fin >> x.length;
+		//cout << "type diametr\n";
+		fin >> x.diametr;
+		fin.close();
+		return x;
 	
 }
-KC createkc()   //создание компрессорной станции
+void PrintPipe(const Pipeline& x)
+{
+	cout << "identification:" << x.ident << endl
+		<< "length:" << x.length << endl
+		<< "diametr:" << x.diametr << endl
+		<< "remont:" << x.remont << endl;
+
+
+}
+
+
+Pipeline inputPipeline()            //создание трубы
+{
+		Pipeline x;
+		x.ident = 0;
+		x.remont = false;
+		cout << "type the length (2000-8000)\n";
+		cin >> x.length;
+		cout << "type the diametr(60-140)\n";
+		cin >> x.diametr;
+		/*do 
+		{
+			cin.clear();
+			cin.ignore(10000, '/n');
+			cout << "type diametr(60-140)\n";
+			cin >> x.diametr;
+		}while (cin.fail()|| x.diametr < 60 || x.diametr > 140);*/
+		return x;
+	}
+//bool IsDcorrect(double d)
+//{
+//	return d >= 60 && d <= 140;
+//}
+
+KC inputkc()   //создание компрессорной станции
 {
 	KC y;
 	cout << "type name kc \n"; //????
@@ -47,24 +84,79 @@ KC createkc()   //создание компрессорной станции
 
 	return y;
 }
-void SavePipeline (const Pipeline& x)
+void SavePipeline(const Pipeline& x)
 {
 	ofstream fout;
 	fout.open("data.txt", ios::out );
-	fout << x.length << endl
-		/*<< "\tDiametr:" */ << x.diametr << endl;
-	fout.close();
+	if (fout.is_open())
+	{
+		fout << x.length << endl
+			/*<< "\tDiametr:" */
+			<< x.diametr << endl
+			<< x.ident << endl
+			<< x.remont << endl;
+		fout.close();
+	}
 }
-//EditPipeline(Pipeline& x)
-//{
-//}
+void EditPipeline(Pipeline& x)
+{
+	x.diametr -= 1;
+	/*x.diametr = IsDcorrect(x.diametr) ? x.diametr : 60;*/
+}
+void PrintMenu()
+{
+	cout << "1. input pipeline" << endl
+		<< "2. print pipeline" << endl
+		<< "3. save to file" << endl
+		<< "4. load from file" << endl
+		<< "5. edit pipeline" << endl
+		<< "0. exit" << endl;
+
+}
 int main()
 {
-	
-	Pipeline pipe1 = LoadPipeline();
-	KC kc1 = createkc();
-	/*EditPipeline(pipe1);*/
-	SavePipeline(pipe1);
+	Pipeline p1;
+	while (1)
+	{
+		PrintMenu();
+		int i = 0;
+		cin >> i;
+		switch (i)
+		{
+		case 1:
+		{
+			p1 = inputPipeline();
+			break;
+		}
+		case 3:
+		{
+			SavePipeline(p1);
+			break;
+		}
+		case 4:
+		{
+			p1 = LoadPipeline();
+			break;
+		}
+		case 5:
+		{
+			EditPipeline(p1);
+			break;
+		}
+		case 0:
+		{
+			return 0;
+			
+		}
+		default	:
+		{cout << "wrong action" << endl; }
+		}
+	}
+	/*Pipeline p1 = LoadPipeline();*/
+	KC kc1 = inputkc();
+	SavePipeline(p1);
+	PrintMenu();
+	EditPipeline(p1);
 	return 0;
 }
 
